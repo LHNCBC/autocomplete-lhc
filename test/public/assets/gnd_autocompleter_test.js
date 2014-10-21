b61357683cbbade81dd9da8a664b270c0482571c
Paul Lynch <plynch@mail.nih.gov>
2014-10-21 18:51:37 -0400

    Rearranged gnd_autocmp to put the mock data in one structure.

--- test/public/assets/gnd_autocompleter_test.js
+++ test/public/assets/gnd_autocompleter_test.js
@@ -1,13 +1,63 @@
+// Make jQuery and PrototypeJS get along
+jQuery.noConflict();
+
+// mockData_ holds the fake AJAX data.  Its structure is
+//   mockData_[fd_id][partial || full || suggest][user text]
+// The "fd_id" is just an identifier put in the URL to distinguish between the
+// different fields.
+mockData_ = {
+  '1284': { // fe_search_cne
+    'partial': {
+      'ar' : '[65,["5398","2910","154","4077","1051","4836","5529"],{"term_icd9_code":["042","518.82","704.01","","284.9","424.1","322.9"]},[["AIDS-related complex"],["Adult respiratory distress syndrome (ARDS)"],["Alopecia areata"],["Androblastoma"],["Anemia - refractory"],["Aortic insufficiency"],["Arachnoiditis"]],false]',
+      'pickw' : '[1,["1848"],{"term_icd9_code":["278.8"]},[["Pickwickian syndrome"]],false]'
+     },
+    'full' : {
+      'ar' :'[65,["5398","2910","154","4077","1051","4836","5529","3406","2119","3140","386","486","2713","1461","4716","615","5692","1610","2266","5652","945","5754","1446","1613","5060","5795","246","2125","5361","1171","4087","685","2709","4631","3040","4347","2636","5448","419","4804","371","3150","4778","5010","1322","4787","86","2232","3922","3052","4271","3971","3537","1087","34","106","204","4521","4477","2898","5545","5860","5868","4977","4656"],{"term_icd9_code":["042","518.82","704.01","","284.9","424.1","322.9","887.2","729.5","427.9","440.9","447.0","","747.60","","","","098.50","696.0","711.90","","","427.5","V45.01","433.10","","414.9","290.40","379.91","446.5","","35.20","","812.20","719.40","714.30","692.4","584.9","","","","403.90","747.0","","","099.3","440.1","799.1","","362.31","390","714.0","959.2","427.9","473.8","","446.7","446.5","524.60","414.00","274.0","","","","427.9"]},[["AIDS-related complex"],["Adult respiratory distress syndrome (ARDS)"],["Alopecia areata"],["Androblastoma"],["Anemia - refractory"],["Aortic insufficiency"],["Arachnoiditis"],["Arm amputation"],["Arm pain"],["Arrhythmia"],["Arteriosclerosis"],["Arteriovenous (AV) fistula"],["Arteriovenous fistula surgery"],["Arteriovenous malformation (AVM)"],["Arteriovenous shunt"],["Artery surgery"],["Arthritis"],["Arthritis - gonococcal"],["Arthritis - psoriatic"],["Arthritis - septic"],["Arthrocentesis"],["Arthroscopy"],["Cardiac arrest"],["Cardiac pacemaker"],["Carotid artery disease"],["Coronary artery angioplasty/stenting"],["Coronary artery disease (CAD)"],["Dementia - multi-infarction"],["Eye pain"],["Giant cell arteritis"],["Hand/arm surgery"],["Heart valve - mechanical"],["Hip arthroplasty - total"],["Humerus fracture"],["Joint pain (arthralgia)"],["Juvenile rheumatoid arthritis (JRA)"],["Keratosis - arsenical"],["Kidney failure (short-term renal failure)"],["Knee arthroplasty - total"],["Lyme arthritis"],["Myeloid maturation arrest"],["Nephrosclerosis - arteriolar"],["Patent ductus arteriosus (PDA)"],["Pulmonary artery - large"],["Pulmonary artery hypertension (PAH)"],["Reiter\'s syndrome"],["Renal artery stenosis"],["Respiratory arrest"],["Resuscitation after cardiac arrest"],["Retinal central artery occlusion"],["Rheumatic fever"],["Rheumatoid arthritis (RA)"],["Shoulder or upper arm injury"],["Sinus arrhythmia"],["Sinus tenderness"],["Spermatogenic arrest"],["Takayasu\'s arteritis"],["Temporal arteritis"],["Temporomandibular arthritis"],["Three vessel coronary artery disease"],["Tophus"],["Transposition of the great arteries (TGA)"],["Truncus arteriosus (TA)"],["Urinary sphincter - artificial"],["Ventricular arrhythmia"]],false]'
+    }
+  },
+  '1285': { // fe_search_button_cne
+    'partial': {
+      'car': '[70,["461","4632","3140","294","5021","1505","5008"],{"term_icd9_code":["199.1","413.9","427.9","35.20","986","680.9","259.2"]},[["Adenosquamous carcinoma"],["Angina pectoris"],["Arrhythmia"],["Bioprosthetic heart valve"],["Carbon monoxide toxicity"],["Carbuncle"],["Carcinoid syndrome"]],false]'
+    }
+  },
+  '2163': { // fe_search_cwe
+    'partial': {
+      'knee' : '[12,["2809","547","419","1486","4145","2031","4795"],{"term_icd9_code":["897.2","897.0","","","","822.0","719.46"]},[["Above knee amputation (AKA)"],["Below knee amputation (BKA)"],["Knee arthroplasty - total"],["Knee derangement - internal"],["Knee dislocation"],["Knee fracture"],["Knee pain"]],false]',
+      'Knee Pain': '[1,["4795"],{"term_icd9_code":["719.46"]},[["Knee pain"]],false]',
+      'ab' : '[87,["3982","2296","2167","9853","9706","4316","13006"],{"term_icd9_code":["789.00","682.9","305.00","626.9","522.5","789.06","634.90"]},[["Abdominal pain"],["Abscess"],["Abuse - alcohol"],["Uterine bleeding - abnormal"],["Tooth abscess"],["Epigastric pain"],["Miscarriage"]],false]',
+      'ar': '[52,["2212","2958","2189","2319","29959","11458","2311"],{"term_icd9_code":["414.9","729.5","379.91","719.40","959.2","584.9","714.0"]},[["Coronary artery disease (CAD)"],["Arm pain"],["Eye pain"],["Joint pain (arthralgia)"],["Shoulder or upper arm injury"],["Kidney failure (short-term renal failure)"],["Rheumatoid arthritis (RA)"]],false]'
+    },
+    'full' : {
+      'ar' : '[52,["2212","2958","2189","2319","29959","11458","2311","9893","21729","5631","18602","9728","2216","2886","8353","6546","1988","6701","22663","5396","3860","8344","2956","2323","11397","8342","10684","9594","8640","8311","8337","11077","4351","10856","10552","5838","9131","11524","13949","10728","11105","11114","10008","10357","4737","8785","2894","8752","3717","X43","X203","X211"],{"term_icd9_code":["414.9","729.5","379.91","719.40","959.2","584.9","714.0","812.20","704.01","427.5","414.00","433.10","427.9","424.1","696.0","711.90","","714.30","290.40","799.1","440.1","524.60","446.5","099.3","427.9","518.82","274.0","473.8","427.9","098.50","446.7","440.9","447.0","284.9","446.5","747.60","042","403.90","322.9","","","692.4","362.31","","390","","747.0","","","","",""]},[["Coronary artery disease (CAD)"],["Arm pain"],["Eye pain"],["Joint pain (arthralgia)"],["Shoulder or upper arm injury"],["Kidney failure (short-term renal failure)"],["Rheumatoid arthritis (RA)"],["Humerus fracture"],["Alopecia areata"],["Cardiac arrest"],["Three vessel coronary artery disease"],["Carotid artery disease"],["Arrhythmia"],["Aortic insufficiency"],["Arthritis - psoriatic"],["Arthritis - septic"],["Pulmonary artery hypertension (PAH)"],["Juvenile rheumatoid arthritis (JRA)"],["Dementia - multi-infarction"],["Respiratory arrest"],["Renal artery stenosis"],["Temporomandibular arthritis"],["Temporal arteritis"],["Reiter\'s syndrome"],["Sinus arrhythmia"],["Adult respiratory distress syndrome (ARDS)"],["Tophus"],["Sinus tenderness"],["Ventricular arrhythmia"],["Arthritis - gonococcal"],["Takayasu\'s arteritis"],["Arteriosclerosis"],["Arteriovenous (AV) fistula"],["Anemia - refractory"],["Giant cell arteritis"],["Arteriovenous malformation (AVM)"],["AIDS-related complex"],["Nephrosclerosis - arteriolar"],["Arachnoiditis"],["Spermatogenic arrest"],["Myeloid maturation arrest"],["Keratosis - arsenical"],["Retinal central artery occlusion"],["Androblastoma"],["Rheumatic fever"],["Arteriovenous shunt"],["Patent ductus arteriosus (PDA)"],["Lyme arthritis"],["Pulmonary artery - large"],["Arthritis"],["Transposition of the great arteries (TGA)"],["Truncus arteriosus (TA)"]],false]'
+    },
+    'suggest' : {
+      'ar' : '[["2886"],[["Aortic insufficiency"]]]'
+    }
+  }
+};
+
 // Mock the Ajax call.  We are only trying to test the JavaScript side here.
 Ajax.Request = function(url, options) {
-  if (url.indexOf('fd_id=1284')>0 && options.parameters['terms'] === 'ar') {
-    var response = {};
-    response.request = this;
-    this.options = options;
-    response.status = 200;
-    response.responseText = '[65,["5398","2910","154","4077","1051","4836","5529"],{"term_icd9_code":["042","518.82","704.01","","284.9","424.1","322.9"]},[["AIDS-related complex"],["Adult respiratory distress syndrome (ARDS)"],["Alopecia areata"],["Androblastoma"],["Anemia - refractory"],["Aortic insufficiency"],["Arachnoiditis"]],false]';
-    setTimeout(function() {options.onComplete(response);}, 1);
+  var params = options.parameters;
+  var resultType = params.autocomp ? 'partial' : params.suggest ? 'suggest' : 'full'
+  // This is just for testing, so assume the right parameters.
+  var fd_id = url.match(/fd_id=(\d+)/)[1];
+  var terms = params.terms || params.field_val; // suggest uses field_val
+  var responseText = mockData_[fd_id][resultType][terms];
+console.log("%%% responseText = "+responseText);
+  if (!responseText) {
+    if (params.suggest === '1')
+      responseText = '[[],[]]';
+    else
+      responseText = '[0,[],{},[],false]';
   }
+
+  var response = {};
+  response.request = this;
+  this.options = options;
+  response.status = 200;
+  response.responseText = responseText;
+  setTimeout(function() {options.onComplete(response);}, 1);
 };
 // end of mock for Ajax.Request
 
--- test/sel_spec.js
+++ test/sel_spec.js
@@ -2,6 +2,10 @@
  *  ATR is a namespace for functions that process the selenese test file.
  */
 ATR = {
+  /**
+   *  Time in milliseconds to wait for something to be occur.
+   */
+  TIMEOUT: 5000,
 
   /**
    *  Returns a function that will run a line of a selenese test file.
@@ -21,16 +25,26 @@ ATR = {
           var arg2 = cmdParts[2].trim();
         }
       }
-      switch(cmd) {
-        case 'open':
-          browser.get(arg1);
-          break;
-        default:
-          if (this.Commands[cmd])
-            this.Commands[cmd](arg1, arg2);
-          else
+      if (this.Commands[cmd])
+        this.Commands[cmd](arg1, arg2);
+      else {
+        switch(cmd) {
+          case 'open':
+            browser.get(arg1);
+            break;
+          case 'mouseDown':
+            this.Commands.click(arg1);
+            break;
+          case 'keyDown':
+            this.Commands.typeKeys(arg1, arg2);
+            break;
+          case 'assertNotVisible':
+            this.Commands.waitForNotVisible(arg1, arg2);
+            break;
+          default:
             throw 'invalid command';
-          break;
+            break;
+        }
       }
     }.bind(ATR);
   },
@@ -48,8 +62,8 @@ ATR = {
       // under the key in arg2.
       if (varName === '')
         throw 'Missing variable name';
-      script = 'if (typeof ATR_testVars_ === "undefined") ATR_testVars_={}; '+
-               'ATR_testVars_["' + varName + '"] = ' + script;
+      script = 'if (typeof window.ATR_testVars_ === "undefined") window.ATR_testVars_={}; '+
+               'window.ATR_testVars_["' + varName + '"] = ' + script;
       browser.driver.executeScript(script);
     },
 
@@ -60,18 +74,46 @@ ATR = {
      * @param expectedVal the expected value for the expression to have.
      */
     assertExpression: function(expression, expectedVal) {
-      var script = ATR.CommandUtil.prepareExpression(expression);
-      // If expectedVal is an integer, convert it.
-      if (expectedVal.match(/^\d+$/))
-        expectedVal = parseInt(expectedVal);
-      // We need to eval the script so that we get the result of the last
-      // expression.  This means we need to put the script in quotes to pass
-      // it as an argument to eval, and that means we need to escape any
-      // double quotes (though we don't actually have those in the current
-      // cases.)
-      script = script.replace('"', '\\"')
-      expect(browser.driver.executeScript('return eval("'+script+'")')).
-             toEqual(expectedVal);
+      expression = ATR.CommandUtil.prepareExpression(expression);
+      expectedVal = ATR.CommandUtil.prepareExpValue(expectedVal);
+      expect(ATR.CommandUtil.remoteEval(expression)).toEqual(expectedVal);
+    },
+
+
+    /**
+     *  Handles a "waitForExpression" command.
+     * @param expression the expression to be evaluated and tested.  The
+     *  expression should be in the form "javascript{expr}" where expr is such
+     *  that one might compare with expectedVal like "expr == expectedVal".
+     * @param expectedVal the expected value for the expression to have.
+     */
+    waitForExpression: function(expression, expectedVal) {
+      expression = ATR.CommandUtil.prepareExpression(expression);
+      expectedVal = ATR.CommandUtil.prepareExpValue(expectedVal);
+      // expectedVal might be a string, in which case we will need to quote it
+      // below, and escape any quotes it contains.  The removeEval function
+      // does the same using double quotes, so we will use single quotes here.
+      if (typeof expectedVal === 'string')
+        expectedVal = '\'' + expectedVal.replace(/\'/g, "\\'") + '\'';
+      browser.wait(function() {
+        return ATR.CommandUtil.remoteEval(expression + '==' + expectedVal);
+      }, ATR.TIMEOUT);
+    },
+
+
+    /**
+     *  Handles a "click" command.
+     * @param locator the ID of the element to receive the event, or a
+     *  selenium-style "locator" string, which can start with "css=" followed by
+     *  a CSS locator.
+     */
+    click: function(locator) {
+      var cssPrefix = 'css=';
+      if (locator.indexOf(cssPrefix) === 0)
+        locator = by.css(locator.substring(cssPrefix.length));
+      else
+        locator = by.id(locator)
+      element(locator).click();
     },
 
 
@@ -82,8 +124,10 @@ ATR = {
      * @param eventType the type of event.
      */
     fireEvent: function(fieldID, eventType) {
-      var elem = element(by.id(fieldID)).getWebElement();
-      browser.driver.actions().mouseDown(elem).perform();
+      if (eventType === 'focus')
+        element(by.id(fieldID)).click();
+      else if (eventType === 'blur')
+        element(by.css('body')).click();
     },
 
 
@@ -94,7 +138,47 @@ ATR = {
      */
     typeKeys: function(fieldID, chars) {
       var elem = element(by.id(fieldID)).getWebElement();
-      elem.sendKeys(chars);
+      if (chars === '\\13')
+        chars = protractor.Key.ENTER;
+      else if (chars === '\\9')
+        chars = protractor.Key.TAB;
+      else if (chars === '\\40')
+        chars = protractor.Key.ARROW_DOWN;
+      else if (chars === '\\27')
+        chars = protractor.Key.ESCAPE;
+      if (this.controlKey_)
+        elem.sendKeys(protractor.Key.CONTROL, chars);
+      else
+        elem.sendKeys(chars);
+    },
+
+
+    /**
+     *  Handles a "type" command (sets a field value).
+     * @param fieldID the ID of the form field
+     * @param val the new field value
+     */
+    type: function(fieldID, chars) {
+      // var elem = element(by.id(fieldID)).getWebElement();
+      // There is probably some way to set a value on the element, but the API
+      // docs are down.
+      ATR.CommandUtil.remoteEval('$("'+fieldID+'").setValue("'+chars+'")');
+    },
+
+
+    /**
+     *  Handles a "typeText" command (sets the field value, and sends key events
+     *  only for the last character).
+     * @param fieldID the ID of the element to receive the event
+     * @param chars the characters to be typed
+     */
+    typeText: function(fieldID, chars) {
+      var charsLen = chars.length;
+      var oneLess = charsLen - 1;
+      var lastChar = chars.substring(oneLess, charsLen);
+      var elem = element(by.id(fieldID)).getWebElement();
+      this.type(fieldID, chars.substring(0, oneLess));
+      this.typeKeys(fieldID, lastChar);
     },
 
 
@@ -105,7 +189,82 @@ ATR = {
     waitForVisible: function(fieldID) {
       var elem = element(by.id(fieldID)).getWebElement();
       expect(elem.isDisplayed()).toBeTruthy();
+    },
+
+
+    /**
+     *  Handles a "waitForNotVisible" command.
+     * @param fieldID the ID of the element that should be visible.
+     */
+    waitForNotVisible: function(fieldID) {
+      var elem = element(by.id(fieldID)).getWebElement();
+      expect(elem.isDisplayed()).toBeFalsy();
+    },
+
+
+    /**
+     *  Handles an "assertValue" command.
+     * @param fieldID the ID of the element that should not have the value.
+     * @param val the value the field should have.
+     */
+    assertValue: function(fieldID, val) {
+      var elem = element(by.id(fieldID)).getWebElement();
+      expect(elem.getAttribute('value')).toBe(val);
+    },
+
+
+     /**
+     *  Handles a "waitForValue" command.
+     * @param fieldID the ID of the element that should not have the value.
+     * @param val the value the field should have.
+     * @param not an optional parameter that flips the test so that it waits
+     *  for the field to not have the value.  (This is used by waitForNotValue.)
+     */
+    waitForValue: function(fieldID, val, not) {
+      if (not === undefined)
+        not = false;
+      var elem = element(by.id(fieldID)).getWebElement();
+      // browser.wait waits for the returned Promise to resolve and be true.
+      browser.wait(function() {
+        return elem.getAttribute('value').then(function(fieldVal) {
+          var rtn;
+          if (not)
+            rtn = fieldVal != val;
+          else
+            rtn = fieldVal == val;
+          return rtn;
+        });
+      }, ATR.TIMEOUT);
+    },
+
+
+    /**
+     *  Handles a "waitForNotValue" command.
+     * @param fieldID the ID of the element that should not have the value.
+     * @param val the value the field shouldn't have.
+     */
+    waitForNotValue: function(fieldID, val) {
+      this.waitForValue(fieldID, val, true);
+    },
+
+
+    /**
+     *  Handles a "controlKeyDown" command.  The key is not actually sent
+     *  until a typeKeys command is issued.
+     */
+    controlKeyDown: function() {
+      this.controlKey_ = true;
+    },
+
+
+    /**
+     *  Handles a "controlKeyUp" command.  The key is not actually sent
+     *  until a typeKeys command is issued.
+     */
+    controlKeyUp: function() {
+      this.controlKey_ = false;
     }
+
   },
 
 
@@ -118,11 +277,59 @@ ATR = {
       // expression is a script wrapped in javascript{...}
       var prefix = 'javascript{';
       if (expression.indexOf(prefix) !== 0) {
-        throw 'Missing prefix: ' + prefix;
+        throw 'Missing prefix: "' + prefix + '" in "'+expression+'"';
       }
       var script = expression.substring(prefix.length, expression.length - 1);
       // Replace storedVals with the actual hash location.
       return script.replace('storedVars', 'ATR_testVars_');
+    },
+
+
+    /**
+     *  Prepares a value for a comparison in an expression test.
+     * @param val the string version of a value
+     * @return the value, converted to its proper type (e.g. true instead of
+     *  'true').
+     */
+    prepareExpValue: function(val) {
+      if (val === 'null')
+        val = null;
+      else if (val === 'true')
+        val = true;
+      else if (val === 'false')
+        val = false;
+      else
+        val = this.makeIntIfInt(val);
+      return val;
+    },
+
+
+    /**
+     *  Converts the given string to an integer if it is one.
+     * @param str the string to possibly convert
+     * @return an integer version of str if string was an integer; otherwise
+     *  str itself.
+     */
+    makeIntIfInt: function(str) {
+      if (str.match(/^\d+$/))
+        str = parseInt(str);
+      return str;
+    },
+
+
+    /**
+     *  Evaluates a JavaScript expression from the selenese, and returns
+     *  its value.
+     * @param expr the expression to evaluate in the browser.
+     */
+    remoteEval: function(expr) {
+      // We need to eval the script so that we get the result of the last
+      // expression.  This means we need to put the script in quotes to pass
+      // it as an argument to eval, and that means we need to escape any
+      // double quotes (though we don't actually have those in the current
+      // cases.)
+      var script = expr.replace(/"/g, '\\"')
+      return browser.driver.executeScript('return eval("'+script+'")');
     }
   }
 };
