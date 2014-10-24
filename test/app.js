a6c34ddb0ac56c486d5e0a1754a7d6c1b8df4af4
Paul Lynch <plynch@mail.nih.gov>
2014-10-24 13:12:36 -0400

    Restricted the node test server to localhost

--- test/app.js
+++ test/app.js
@@ -3,6 +3,15 @@ var connect = require('connect'),
     http = require('http');
 
 connect()
+    .use(function(req, resp, next) {
+        if(req.connection.remoteAddress !== '127.0.0.1') {
+          resp.statusCode = 403; // forbidden
+          resp.end('403 Forbidden');
+        }
+        else {
+          next();
+        }
+      })
     .use(connect.static('.'))
     .use(connect.directory('.'))
     .listen(3000);
