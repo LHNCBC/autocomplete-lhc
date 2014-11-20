73278668e29b72409f776d44843e75804bb664ee
Paul Lynch <plynch@mail.nih.gov>
2014-11-20 18:34:31 -0500

    Replaced the localhost restriction of test/app.js with a refusal to serve files starting with .

--- test/app.js
+++ test/app.js
@@ -4,14 +4,16 @@
 //
 // Based on http://stackoverflow.com/a/12019645/360782
 var connect = require('connect'),
-    http = require('http');
+    http = require('http'),
+    path = require('path');
 
 // Set the document root to the package directory, which is the
 // parent of the directory containing this script.
 var docRoot = __dirname + '/..';
 
 connect()
-    .use(function(req, resp, next) {
+/*    .use(function(req, resp, next) {
+        // Restrict access to localhost
         var remoteIP = req.connection.remoteAddress;
         if(remoteIP !== '127.0.0.1' && remoteIP !== '::1') {
           resp.statusCode = 403; // forbidden
@@ -20,7 +22,23 @@ connect()
         else {
           next();
         }
-      })
+      })*/
+    .use(function(req, resp, next) {
+       // block access to files and directories beginning with '.'
+       var p = req.url;
+       var pathOkay = true;
+       while (pathOkay && p !== '/') {
+         if (path.basename(p).indexOf('.') === 0) {
+           resp.statusCode = 403; // forbidden
+           resp.end('403 Forbidden');
+           pathOkay = false;
+         }
+         else
+           p = path.dirname(p);
+       }
+       if (pathOkay)
+         next();
+     })
     .use(connect.static(docRoot))
     .use(connect.directory(docRoot))
     .listen(3000);
