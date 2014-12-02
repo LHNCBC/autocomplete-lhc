eb9604ee08db219347ccbfef9134d27d37b36cb0
Paul Lynch <plynch@mail.nih.gov>
2014-12-02 10:44:04 -0500

    Now finds the error sound file based on the path to the autocompleter JS

--- fieldAlarms.js
+++ fieldAlarms.js
@@ -9,11 +9,21 @@ Def.FieldAlarms = {
    **/
   setOffAlarm: function(field) {
     if (this.bonk === undefined)
-      this.bonk = new Audio('/soundmanager/bonk.mp3')
+      this.bonk = new Audio(this.soundFile_);  // see below for soundFile_
     // Reset the play position back the the beginning, if the sound has
     // been loaded sufficiently.
-    if (this.bonk.readyState >= 2)
-      this.bonk.currentTime = 0;
+    if (this.bonk.readyState >= 2) {
+      this.bonk.currentTime = 0; // now the sound can be replayed
+      if (this.bonk.currentTime !== 0) {
+        // Work around Chrome bug.  However, this bug really
+        // has to do with the server not setting the Accept-Ranges
+        // and Content-Range headers on the response.  The drawback
+        // of this workaround is that it will trigger a reload
+        // of the sound file (so it is better to adjust your server's
+        // configuration if you are having that problem).
+        this.bonk.src = this.bonk.src; // should reset the time to 0
+      }
+    }
     this.bonk.play();
     Effect.Shake(field.id, 5);
   }, // end of setOffAlarm
@@ -27,4 +37,14 @@ Def.FieldAlarms = {
     this.bonk.currentTime = 0;
   }
 };
+
+
+// Determine the path to the this script, so we can find the sound file.
+// See http://ejohn.org/blog/degrading-script-tags/
+(function() {
+  var scripts = document.getElementsByTagName("script");
+  var thisURL = scripts[scripts.length - 1].src;
+  var path = thisURL.substring(0, thisURL.lastIndexOf('/'));
+  Def.FieldAlarms.soundFile_ = path + '/soundmanager/bonk.mp3'
+})();
 
