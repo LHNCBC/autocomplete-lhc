82058a34a6be88b3f3edd8635028cb20633dd145
Paul Lynch <plynch@mail.nih.gov>
2015-01-23 14:36:57 -0500

    Removed phrAutocompleteConfig from the directive

--- phr-autocomplete.js
+++ phr-autocomplete.js
@@ -1,29 +1,56 @@
 'use strict';
 // An AngularJS directive (optional; for use if you are using AngularJS)
+//
+// Example:
+// <input name="myfield" phr-autocomplete="opts" ng-model="selectedVal">
+//
+// The opts object (which could be a function that returns an object) contains
+// the information needed for specifying the behavior of the autocompleter (e.g.
+// what should be in the list).  There are two types of autocompleters.  You can
+// either have a "prefetched" list where all the items are given to the autocompleter
+// at construction time, or a "search" list where the list items are discovered
+// as the user types, via AJAX calls.  For both types, opts is a hash, but the
+// keys on the hash differ.
+//
+// For "prefetched lists", opts can contain:
+// 1) source - (required) This is the list item data.  It should be an array,
+//    and each element in the array should have a "label" property which is the
+//    display string the user sees.  The object containing that label property
+//    is what will get stored on the model you have associated via ng-model
+//    (selectedVal, in the example above).
+// 2) maxSelect - (default: 1) the maximum number of items that can be selected
+//    from the list.  If this is '*', an unlimited number can be selected.  When
+//    more than one item can be selected, selected items are stored in an array
+//    on the model (e.g., selectedVal becomes an array).
+// 3) Any other parameters used by the Def.Autocomp.Prefetch constructor defined in
+//    autoCompPrefetch.js.  (Look at the options parameter in the initialize method).
+//
+// For "search" lists, opts can contain:
+// 1) url - (required) The URL to which requests for data should be sent.  For
+//    details about expected parameters and response data, see the comments for the
+//    Def.Autocomp.Search constructor (the initialize method in autoCompSearch.js.)
+// 2) Any other parameters used by the Def.Autocomp.Search constructor defiend
+//    in autoCompSearch.js.  (Look at the options parameter in the initialize
+//    method.)
 if (typeof angular !== 'undefined') {
   angular.module('autocompPhr', [])
 
-    .constant('phrAutocompleteConfig', {})
-
-    .directive('phrAutocomplete', ['phrAutocompleteConfig', '$timeout', function (phrAutocompleteConfig, $timeout) {
+    .directive('phrAutocomplete', ['$timeout', function (phrAutocompleteConfig, $timeout) {
       'use strict';
-      var options;
-      options = {};
-      angular.extend(options, phrAutocompleteConfig);
       return {
         require:'?ngModel',
         scope: {
-          modelData: '=ngModel'
+          modelData: '=ngModel' // the ng-model attribute on the tag
         },
         link:function (scope, element, attrs, controller) {
           var getOptions = function () {
-            // Because we created our own scope, we have to evaluate
-            // attrs.phrAutocomplete in the parent scope.
-            return angular.extend({}, phrAutocompleteConfig, scope.$parent.$eval(attrs.phrAutocomplete));
+            return angular.extend({}, phrAutocompleteConfig, );
           };
 
           var initWidget = function () {
-            var phrAutoOpts = getOptions();
+            // Because we created our own scope, we have to evaluate
+            // attrs.phrAutocomplete in the parent scope.
+            var phrAutoOpts = scope.$parent.$eval(attrs.phrAutocomplete);
 
             // If we have a controller (i.e. ngModelController) then wire it up
             if (controller) {
@@ -117,7 +144,8 @@ if (typeof angular !== 'undefined') {
             } // if controller
           };
 
-          // Run initWiget once after the name attribute has been filled in
+          // Run initWiget once after the name attribute has been filled in (for
+          // situations where the name is coming from the data model).
           scope.$watch({}, initWidget, true); // i.e. run once
         }
       };
