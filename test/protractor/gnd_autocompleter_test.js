0652a2a0ad73ac1604bd677f6be1d6ff76c235db
Paul Lynch <plynch@mail.nih.gov>
2015-03-04 14:03:17 -0500

    Changed gnd_autocompleter_test.js to use the new constants for the suggestionMode option, and corrected the option setting to match the previous behavior the test was expecting

--- test/protractor/gnd_autocompleter_test.js
+++ test/protractor/gnd_autocompleter_test.js
@@ -4,12 +4,12 @@ jQuery.noConflict();
 var opts = {};
 opts['matchListValue']=true
 opts['codes']=["LA6155-1","LA6156-9","LA6162-7","LA6214-6","LA6266-6","LA4457-3","LA4489-6"]
-opts['suggestionMode']=0
+opts['suggestionMode']=Def.Autocompleter.SUGGEST_SHORTEST;
 opts['autoFill']=true
 var fe_race_or_ethnicity_autoComp = new Def.Autocompleter.Prefetch('fe_race_or_ethnicity', ["American Indian or Alaska Native","Asian","Black or African-American","Hispanic or Latino","Native Hawaiian or Pacific Islander","White","Unknown"], opts);
 var opts = {};
 opts['codes']=["LA44-3","LA45-0","LA46-8"]
-opts['suggestionMode']=0
+opts['suggestionMode']=Def.Autocompleter.SUGGEST_SHORTEST;
 opts['autoFill']=true
 var fe_prefetch_cwe_autoComp = new Def.Autocompleter.Prefetch('fe_prefetch_cwe', ["Spanish","French","Other"], opts);
 var opts = {};
@@ -19,17 +19,20 @@ var fe_search_cne_autoComp =
   new Def.Autocompleter.Search('fe_search_cne',
     '/form/get_search_res_list?fd_id=1284', opts);
 
-var opts = {'matchListValue': false, 'suggestionMode': 1, 'autocomp': true};
+var opts = {'matchListValue': false,
+ 'suggestionMode': Def.Autocompleter.SUGGEST_SHORTEST, 'autocomp': true};
 var fe_search_cwe_autoComp =
   new Def.Autocompleter.Search('fe_search_cwe',
     '/form/get_search_res_list?fd_id=2163', opts);
 
-var opts = {'matchListValue': false, 'suggestionMode': 0, 'autocomp': true};
+var opts = {'matchListValue': false,
+ 'suggestionMode': Def.Autocompleter.NO_COMPLETION_SUGGESTIONS, 'autocomp': true};
 var fe_search_cwe_autoComp =
   new Def.Autocompleter.Search('fe_search0_cwe',
     '/form/get_search_res_list?fd_id=2163', opts);
 
-var opts = {'matchListValue': false, 'suggestionMode': 2, 'autocomp': true};
+var opts = {'matchListValue': false,
+ 'suggestionMode': Def.Autocompleter.USE_STATISTICS, 'autocomp': true};
 var fe_search_cwe_autoComp =
   new Def.Autocompleter.Search('fe_search2_cwe',
     '/form/get_search_res_list?fd_id=2163', opts);
@@ -37,19 +40,19 @@ var fe_search_cwe_autoComp =
 var opts = {};
 opts['matchListValue']=true
 opts['codes']=["LA6155-1","LA6156-9","LA6162-7","LA6214-6","LA6266-6","LA4457-3","LA4489-6"]
-opts['suggestionMode']=0
+opts['suggestionMode']=Def.Autocompleter.SUGGEST_SHORTEST;
 opts['autoFill']=true
 var fe_table_prefetch_cne_autoComp = new Def.Autocompleter.Prefetch('fe_table_prefetch_cne', ["LA6155-1 - American Indian or Alaska Native","LA6156-9 - Asian","LA6162-7 - Black or African-American","LA6214-6 - Hispanic or Latino","LA6266-6 - Native Hawaiian or Pacific Islander","LA4457-3 - White","LA4489-6 - Unknown"], opts);
 var opts = {};
 opts['matchListValue']=true
-opts['suggestionMode']=0
+opts['suggestionMode']=Def.Autocompleter.SUGGEST_SHORTEST;
 opts['buttonID']='fe_search_button_cne_button'
 opts['autocomp']=true
 var fe_search_button_cne_autoComp =
   new Def.Autocompleter.Search('fe_search_button_cne',
     '/form/get_search_res_list?fd_id=1285', opts);var opts = {};
 opts['matchListValue']=true
-opts['suggestionMode']=0
+opts['suggestionMode']=Def.Autocompleter.SUGGEST_SHORTEST;
 opts['autocomp']=true
 var fe_table_search_cne_autoComp =
   new Def.Autocompleter.Search('fe_table_search_cne',
