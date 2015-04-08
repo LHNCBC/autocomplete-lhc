42ae87e1072ac0e889550831b22e6f677341f731
Paul Lynch <plynch@mail.nih.gov>
2015-04-08 18:44:24 -0400

    Updated comments per review

--- test/protractor/spec/scrollTests.js
+++ test/protractor/spec/scrollTests.js
@@ -2,7 +2,7 @@ var po = require('../autocompPage.js');
 describe('autocomp scroll function', function() {
   it('should scroll the list into view', function() {
     po.openTestPage();
-    browser.manage().window().setSize(1100, 473); // 323 = just below #long_odd_cne
+    browser.manage().window().setSize(1100, 473); // height just includes #long_odd_cne
     expect(po.windowScrollTop()).toBe(0);
     po.longOddCNE.click();
     expect(po.windowScrollTop()).toBeGreaterThan(0);
@@ -11,7 +11,7 @@ describe('autocomp scroll function', function() {
 
   it('should not scroll the list into view when that is disabled', function() {
     po.openTestPage();
-    browser.manage().window().setSize(1100, 473); // 323 = just below #long_odd_cne
+    browser.manage().window().setSize(1100, 473); // height just includes #long_odd_cne
     expect(po.windowScrollTop()).toBe(0);
     po.longOddCNENoScroll.click();
     expect(po.windowScrollTop()).toBe(0);
