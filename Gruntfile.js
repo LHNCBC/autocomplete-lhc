5e6903d248725ba398bca764d20b6d5dacb560fd
Paul Lynch <plynch@mail.nih.gov>
2015-04-09 13:14:17 -0400

    Removed bower_components; copied needed files to dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,11 @@ module.exports = function(grunt) {
 
     copy: {
       dist: {
-        files: [ {src: 'indexPreMin.html', dest: 'index.html'} ]
+        files: [ {src: 'indexPreMin.html', dest: 'index.html'},
+                 {src: ['autocomplete-lhc/*.css',
+                        'autocomplete-lhc/*.png',
+                        'jquery-ui/themes/ui-lightness/**'],
+                  dest: 'dist/', expand: true, cwd: 'bower_components'} ]
       }
     },
 
