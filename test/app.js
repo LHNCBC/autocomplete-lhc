af1b37458b8f5e5d5c1f4bb3a3fe535591b2c4ae
Paul Lynch <plynch@mail.nih.gov>
2015-10-29 10:49:01 -0400

    Removed accidental check-in of .swp file, and adjusted comments in app.js.

--- test/app.js
+++ test/app.js
@@ -1,6 +1,6 @@
 // A simple web server to serve the test files.
 // Start with:  node app.js
-// and browse to port 3000 from localhost.
+// and browse to port 3004 from localhost.
 //
 // Based on http://stackoverflow.com/a/12019645/360782
 var connect = require('connect'),
@@ -13,17 +13,6 @@ var config = require('./config');
 var docRoot = __dirname + '/..';
 
 connect()
-/*    .use(function(req, resp, next) {
-        // Restrict access to localhost
-        var remoteIP = req.connection.remoteAddress;
-        if(remoteIP !== '127.0.0.1' && remoteIP !== '::1') {
-          resp.statusCode = 403; // forbidden
-          resp.end('403 Forbidden');
-        }
-        else {
-          next();
-        }
-      })*/
     .use(function(req, resp, next) {
        // block access to files and directories beginning with '.'
        var p = req.url;
