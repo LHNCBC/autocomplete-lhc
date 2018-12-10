00fff82ed1b610f76c7b792a9386e99814d208a1
Paul Lynch <plynch@mail.nih.gov>
2018-12-10 13:02:15 -0500

    Grunt is now just used to run protractor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,11 @@
 module.exports = function(grunt) {
   // Load grunt tasks automatically, when needed
   require('jit-grunt')(grunt, {
-    compress: 'grunt-contrib-compress',
-    copy: 'grunt-contrib-copy',
-    cssmin: 'grunt-contrib-cssmin',
-    nsp: 'grunt-nsp',
-    protractor: 'grunt-protractor-runner',
-    shell: 'grunt-shell',
-    uglify: 'grunt-contrib-uglify'
+    protractor: 'grunt-protractor-runner'
   });
-
-  var wiredep = require('wiredep');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    clean: ['dist'],
-
-    compress: {
-      main: {
-        options: {
-          archive: '<%= uncompressedDist %>.zip'
-        },
-        files: [{
-          src: ['<%= versionedName %>/**'],
-          cwd: 'dist',
-          expand: true
-        }]
-      }
-    },
-
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          flatten: true,
-          src: ['source/*png', 'LICENSE.md', 'README-dist.md'],
-          dest: '<%= uncompressedDist %>'
-        }]
-      }
-    },
-
-
-    cssmin: {
-      target: {
-        files: [
-          {
-            src: ['source/auto_completion.css'],
-            dest: '<%= uncompressedDist %>/autocomplete-lhc.min.css',
-          }
-        ]
-      }
-    },
-
-
-    mochaTest: {
-      options: {
-        reporter: 'spec'
-      },
-      src: ['test/mocha/*.js']
-    },
-
-
-    nsp: {
-      package: grunt.file.readJSON('./package.json'),
-      shrinkwrap: grunt.file.readJSON('./npm-shrinkwrap.json')
-    },
-
 
     protractor: {
       options: {
@@ -78,73 +17,6 @@ module.exports = function(grunt) {
           }
         }
       }
-    },
-
-
-    shell: {
-      dist_dir_link: {
-        // Make a softlink to the versioned dist directory, for the tests
-        command: 'ln -s <%= versionedName %> latest',
-        options: {
-          execOptions: {
-            cwd: 'dist'
-          }
-        }
-      },
-      run_tests: {
-        command: './test/run_tests.sh'
-      },
-      run_e2e_tests: {
-        command: './test/run_tests.sh skip_unit_tests'
-      }
-    },
-
-
-    uglify: {
-      options: { compress: true },
-      my_target: {
-        files: {
-          // Minified version of just the autocomplete-lhc files
-          '<%= uncompressedDist %>/autocomplete-lhc.min.js':
-            wiredep({includeSelf: true, exclude: [/jquery/]}).js,
-          // Minified version of autocomplete-lhc and all its dependencies
-          '<%= uncompressedDist %>/autocomplete-lhc_jQuery.min.js':
-            wiredep({includeSelf: true, exclude: [/jquery-ui\.js/]}).js
-        }
-      }
     }
-
   });
-
-
-  grunt.registerTask('readBowerVersion', function () {
-    var bowerVersion = grunt.file.readJSON('./bower.json').version;
-    var versionedName = 'autocomplete-lhc-'+bowerVersion;
-    grunt.config.set('versionedName', versionedName);
-    grunt.config.set('uncompressedDist', 'dist/'+versionedName);
-  });
-
-  grunt.registerTask('compressDist', ['readBowerVersion',
-    'compress']);
-
-//  grunt.registerTask('dist', ['clean', 'readBowerVersion', 'copy:dist', 'cssmin',
-//        'uglify', 'compress', 'shell:dist_dir_link']);
-// switch to npm run build
-
-  grunt.registerTask('test', ['dist', 'shell:run_tests']);
-
-  grunt.registerTask('test:e2e', ['shell:run_e2e_tests']);
-
-  // This task is just for debugging the "uglify" configuration
-  grunt.registerTask('listDepJS', function() {
-    console.log("\n\n" + wiredep(
-      {includeSelf: true, exclude: [/jquery/]}).js.join("\n"));
-    console.log("\n\n" + wiredep(
-      {includeSelf: true, exclude: [/jquery(-ui)?\.js/]}).js.join("\n"));
-    console.log("\n\n" + wiredep(
-      {includeSelf: true, exclude: [/jquery-ui\.js/]}).js.join("\n"));
-    console.log("\n\n" + wiredep(
-      {includeSelf: true}).js.join("\n"));
-  });
-
 };
